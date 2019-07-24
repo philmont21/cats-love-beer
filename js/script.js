e74a86181342cb39b91d489b500df36fc65609bb
Lucas Sinclair <lucas.j.sinclair@Gmail.com>
2019-07-23 21:35:00 -0400

    updated javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var agePrompt=prompt("What is your age?");
+ 
+if (agePrompt>=21) alert('Come On In and Enjoy!')
+else {
+alert('Nope! Not Till You Are Older!')
+window.location.href = "http://www.sesamestreet.org";;
+ 
+}
