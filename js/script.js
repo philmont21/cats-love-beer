96416200b7bcb7ae0236b1b947c359b723cda797
Lucas Sinclair <lucas.j.sinclair@Gmail.com>
2019-07-23 22:56:55 -0400

    updated javascript text

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Although this is not exactly a fool-proof method of age verification, the redirect is always fun ;) //
 
-var agePrompt=prompt("What is your age?");
+var agePrompt=prompt("How Old Are You??");
  
 if (agePrompt>=21) alert('Come On In and Enjoy!')
 else {
-alert('Nope! Not Till You Are Older!')
-window.location.href = "http://www.sesamestreet.org";
- 
+    alert('Nope! Come back in a few years!')
+    window.location.href = "http://www.sesamestreet.org";
+    
 }
